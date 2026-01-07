c802f761e986e3f05f5fd354458300536f09ac62
Disha Chakraborty <chakrabortydisha809@gmail.com>
2026-01-07 22:50:59 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ImageUpload from "./components/ImageUpload";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <ImageUpload />
+    </div>
+  );
+}
+
+export default App;
